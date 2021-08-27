cfa8b7b85ec3536a32e99309fba9389aad4d512e
kahjee <89579589+kahjee@users.noreply.github.com>
2021-08-27 11:28:15 +0800

    Add files via upload

---
+++ OOP-Module-1-Exam-Maruyama.dart
@@ -0,0 +1,26 @@
+//abstraction is the concept of simplification.
+//encapsulation is the concept of bundling data in the form of classes.
+//inheritance is the concept of sharing from classes, and keeping them secure until needed.
+//polymorphism is the concept of objects being used in multiple ways.
+
+import 'dart:ffi';
+
+class Languages {
+  void eselanguages(){
+    
+    var eastasia = ["Putong-hua", "Nihon-go", "Gwong-zau-waa", "Hangug-eo"];
+    var southeastasia = ["Filipino", "Bahasa Melayu", "Bahasa Indonesia", "Tieng Viet"];
+    String ispeak = southeastasia[0] + eastasia[1];
+    print(ispeak);
+    
+  }
+
+  void main(){
+    
+    eselanguages();
+    print("Hello.");
+
+  }
+}
+
+// in this program, the 4 principles have been applied. 
